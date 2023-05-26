76fa5f8f82b2ef7995393dcd0807ba782b21286d
Mihai00-roacs <56954718+Mihai00-roacs@users.noreply.github.com>
2023-05-26 09:43:18 +0300

    Add files via upload

---
+++ EmailSender.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace DataAccess
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void SendEmail(string email, string subject, string htmlMessage)
+        {
+            string fromMail = _config["CommonSettings:SendNotificationMail"];
+            string fromPassword = _config["CommonSettings:SendNotificationPassword"];
+
+            MailMessage message = new();
+            message.From = new MailAddress(fromMail);
+            message.Subject = subject;
+            message.To.Add(new MailAddress(email));
+            message.Body = "<html><body><div> <h1>Hello There!</h1> </div> <div> Welcome To Our Site. Please follow this link to activate your account : <a href=\" " + htmlMessage + " \"> Validate your account</a> </div>   </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false
+
+            };
+            smtpClient.Send(message);
+        }
+
+        public void SendEmailPassword(string email, string subject, string htmlMessage)
+        {
+            string fromMail = _config["CommonSettings:SendNotificationMail"];
+            string fromPassword = _config["CommonSettings:SendNotificationPassword"];
+            MailMessage message = new();
+            message.From = new MailAddress(fromMail);
+            message.Subject = subject;
+            message.To.Add(new MailAddress(email));
+            message.Body = "<html><body><div> <h1>Hello There!</h1> </div> <div> We are sorry that you Lost your Password. Please follow this link to use a new one : <a href=\" " + htmlMessage + " \">Reset Password </a> </div>   </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+            smtpClient.Send(message);
+
+        }
+    }
+}
---
+++ IEmailSender.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess
+{
+    public interface IEmailSender
+    {
+        void SendEmail(string email, string subject, string htmlMessage);
+        void SendEmailPassword(string email, string subject, string htmlMessage);
+    }
+}
